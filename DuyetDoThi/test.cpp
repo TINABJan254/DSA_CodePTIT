d7f674d859658b07e06db0c467b9f4ca1989f99a
Tinab <trandinhthien0104@gmail.com>
2024-06-22 18:30:03 +0700

    test gitignore

--- DuyetDoThi/test.cpp
+++ DuyetDoThi/test.cpp
@@ -1,42 +1,208 @@
-#include<bits/stdc++.h>
-using namespace std;
+//Dijkstra
+int a[105][105], u, n, d[105], e[105], vs[105];
+void DIJKSTRA(int u)
+{
+    // khởi tạo
+    for (int v = 1; v <= n; v++) {
+        d[v] = a[u][v]; e[v] = u;
+    }
+    // bắt đầu tìm kiếm từ đỉnh u d[u] = 0;
+    e[u] = 0;
+    vs[u] = 1;
 
-using ll=long long;
-#define pb push_back
-#define sz size()
-#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-#define vi vector<int>
-#define vll vector<ll>
+    while (1){
+        int s = 0, min = 100000;
+        // tìm đỉnh s sao cho d[s] min và vs[s] = 0 
+        for (int v = 1; v <= n; v++) {
+            if (vs[v] == 0 && d[v] < min){
+                min = d[v]; 
+                s = v;
+            }
+        }
+        if (s == 0) // nếu không tìm được đỉnh s thì kết thúc
+            return;
 
-int mod=1e9+7;
-int main(){
-    faster();
-    queue<ll> q;
-    vll v;
-    q.push(1);
-    while(!q.empty()){
-    	ll u=q.front();
-    	q.pop();
-    	v.pb(u);
-    	if(u<=1e19){
-    		q.push(10*u);
-		    q.push(10*u+1);
-		}
-	}
-	int t;
-	cin>>t;
-	while(t--){
-		int cnt=0;
-		int n;
-		cin>>n;
-		for(ll x:v){
-			if(x<=n){
-				++cnt;
-			}
-			else{
-				break;
-			}
-		}
-		cout<<cnt<<endl;
-	}
+        vs[s] = 1; // đánh dấu đỉnh s
+
+        for (int v = 1; v <= n; v++){
+            if (vs[v] == 0 && d[v] > d[s] + a[s][v]){
+                d[v] = d[s] + a[s][v]; 
+                e[v] = s;
+            }
+        }
+    }
+}
+
+
+//Bellman-Ford
+int n, s, a[100][100], d[100], e[100];
+
+int BellmanFord(int s) {
+    int dem, u, v;
+
+    for (v = 1; v <= n; v++) {
+        d[v] = a[s][v];
+        e[v] = s;
+    }
+    d[s] = 0;
+    e[s] = 0;
+
+    int ok = 0;
+    for (dem = 1; dem <= n - 1; dem++) {
+        int ok = 1;
+        for (v = 1; v <= n; v++) {
+            for (u = 1; u <= n; u++) {
+                if (d[v] > d[u] + a[u][v]) {
+                    d[v] = d[u] + a[u][v];
+                    e[v] = u;
+                    ok = 0;
+                }
+            }
+        }
+        if (ok == 1) return 1;
+    }
+    return 0;
+}
+
+
+//Floyd
+int n, a[100][100], d[100][100], e[100][100];
+
+int Floyd() {
+    int i, j, k;
+    
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            d[i][j] = a[i][j];
+            e[i][j] = i;
+        }
+    }
+
+    for (k = 1; k <= n; k++) {
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n; j++) {
+                if (d[i][j] > d[i][k] + d[k][j]) {
+                    d[i][j] = d[i][k] + d[k][j];
+                    e[i][j] = k;
+                }
+            }
+        }
+    }
+
+    for (i = 1; i <= n; i++) {
+        if (d[i][i] < 0) return 0;
+    }
+    
+    return 1;
+}
+//Kruskal
+
+int n, m, d[10000], c[10000], ts[10000];
+int vs[100], t[100];
+
+void Kruskal() {
+    // Sort edges by weight
+    for (int i = 1; i <= m - 1; i++) {
+        for (int j = i + 1; j <= m; j++) {
+            if (ts[i] > ts[j]) {
+                swap(ts[i], ts[j]);
+                swap(d[i], d[j]);
+                swap(c[i], c[j]);
+            }
+        }
+    }
+
+    int wt = 0, k = 0;
+    for (int i = 1; i <= n; i++) {
+        vs[i] = 0;
+    }
+
+    for (int i = 1; i <= m; i++) {
+        if (!(vs[d[i]] != 0 && vs[d[i]] == vs[c[i]])) {
+            k++;
+            t[k] = i;
+            wt += ts[i];
+            if (k == n - 1) {
+                cout << wt << endl;
+                for (int j = 1; j <= k; j++) {
+                    cout << d[t[j]] << " " << c[t[j]] << endl;
+                }
+                return;
+            }
+            int u = d[i], v = c[i];
+            if (vs[u] == 0 && vs[v] == 0) {
+                vs[u] = k;
+                vs[v] = k;
+            } else if (vs[u] == 0 && vs[v] != 0) {
+                vs[u] = vs[v];
+            } else if (vs[u] != 0 && vs[v] == 0) {
+                vs[v] = vs[u];
+            } else if (vs[u] < vs[v]) {
+                int tg = vs[v];
+                for (int j = 1; j <= n; j++) {
+                    if (vs[j] == tg) vs[j] = vs[u];
+                }
+            } else if (vs[v] < vs[u]) {
+                int tg = vs[u];
+                for (int j = 1; j <= n; j++) {
+                    if (vs[j] == tg) vs[j] = vs[v];
+                }
+            }
+        }
+    }
+}
+
+
+//Prim
+int n, a[100][100];
+int vs[100], d[100], e[100];
+
+void Prim(int s) {
+    for (int v = 1; v <= n; v++) {
+        vs[v] = 0;
+        d[v] = a[s][v];
+        e[v] = s;
+    }
+
+    vs[s] = 1;
+    d[s] = 0;
+    e[s] = 0;
+    int wt = 0, dem = 1;
+
+    while (dem < n) {
+        int u = 0;
+        int min = INT_MAX;
+
+        for (int v = 1; v <= n; v++) {
+            if (vs[v] == 0 && d[v] < min) {
+                min = d[v];
+                u = v;
+            }
+        }
+
+        if (u == 0) {
+            cout << "Khong co cay khung" << endl;
+            return;
+        }
+
+        vs[u] = 1;
+        wt += a[u][e[u]];
+        dem++;
+
+        for (int v = 1; v <= n; v++) {
+            if (vs[v] == 0 && d[v] > a[u][v]) {
+                d[v] = a[u][v];
+                e[v] = u;
+            }
+        }
+    }
+
+    cout << wt << endl;
+    for (int v = 1; v <= n; v++) {
+        if (e[v] != 0) {
+            cout << v << " " << e[v] << endl;
+        }
+    }
+
+    return;
 }
--- DuyetDoThi/test2.cpp
+++ DuyetDoThi/test2.cpp
@@ -1,85 +1,110 @@
-#include <bits/stdc++.h>
+int a[100][100];
+int u, n, d[100], e[100], vs[100];
 
-using namespace std;
-
-#define EL "\n"
-#define ll long long
-#define fi first
-#define se second
-#define sz size()
-#define pb push_back
-#define vi vector<int>
-#define vii vector<vector<int>>
-#define faster() ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-#define pii pair<int, int>
-
-const int MOD = 1e9 + 7;
-const int N = 1e6 + 5;
-
-int n, m;
-int parent[1005], Sz[1005];
-
-void make_set(){
-	for (int i = 1; i <= n; i++){
-		parent[i] = i;
-		Sz[i] = 1;
+void Dijkstra(int u){
+	//Khoi tao
+	for (int v = 1; v <= n; v++){
+		vs[v] = 0;
+		d[v] = a[u][v];
+		e[v] = u;
 	}
+	e[u] = 0; d[u] = 0;
+	
+	while (1){
+		int s = 0, min = INT_MAX;
+		for (int i = 1; i <= n; i++){
+			if (d[i] < min){
+				min = d[i];
+				s = i;
+			}
+		}
 		
-}
-
-//Di tim dai dien cho dinh u
-int find_set(int u){
-	if (u == parent[u])
-		return u;
-	return parent[u] = find_set(parent[u]);
-	/*
-	int p = find_set(parent[u]);
-	parent[u] = p;
-	return p;
-	*/
-}
-
-bool union_sets(int x, int y){
-	x = find_set(x);
-	y = find_set(y);
-	if (x != y){ 
-		if (Sz[x] < Sz[y]) swap(x, y);
-		parent[y] = x; //Chon dai dien cua tap co nhieu phan tu hon lam dai dien cho tap con lai
-		Sz[x] += Sz[y];
-		return true;
-		/*
-		if (Sz[x] > Sz[y]){
-			parent[y] = x;
-			Sz[x] += Sz[y];
+		if (s == 0)
+			return;
+		
+		vs[s] = 1;
+		for (int v = 1; v <= n; v++){
+			if (d[v] > d[s] + a[s][v])
+				d[v] = d[s] + a[s][v];
+				e[v] = s;
 		}
-		else{
-			parent[x] = y;
-			Sz[y] += Sz[x];
-		}
-		*/
 	}
-	return false;
 }
 
-void solve(){
-	cin >> n >> m;
-	make_set();
-	for (int i = 0; i < m; i++){
-		int x, y; cin >> x >> y;
-		union_sets(x, y);
+
+//bellman-ford
+int a[100][100], d[100], e[100];
+void bellm(int s){
+	//ktao
+	for (int v = 1; v <= n; v++){
+		d[v] = a[s][v];
+		e[v] = s;
 	}
 	
-	for (int i = 1; i <= n; i++)
-		if (i == parent[i])
-			cout << i << ' ';
+	d[s] = 0; e[s] = 0;
+	
+	for (int k = 1; k <= n - 2; k++){
+		for (int v = 1; v <= n; v++)
+			for (int u = 1; u <= n; u++){
+				if (d[v] > d[u] + a[u][v]){
+					d[v] = d[u] + a[u][v];
+					e[v] = u;
+				}
+			}
+	}
 }
 
-int main(){
-	#ifndef ONLINE_JUDGE
-		freopen("inputf.txt", "r", stdin);
-	#endif
-	faster();
-	solve();
-	return 0;
+//Floyd
+int a[100][100], d[100][100], e[100][j];
+int n, m;
+
+int Floyd(){
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++){
+			d[i][j] = a[i][j]
+			e[i][j] = i;
+		}
+		
+	for (int k = 1; k <= n; k++)
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++)
+				if (d[i][j] > d[i][k] + d[k][j]){
+					d[i][j] = d[i][k] + d[k][j];
+					e[i][j] = k;
+				}
 }
+
+//kruskal
+vs, d, c, ts;
+
+void kruskal(){
+	//sort edge
+	for (int i = 1; i <= m - 1; i++)
+		for (int j = i + 1; j <= m; j++)
+			if (t[i] > t[j])
+				swap(edge_j, edge_i);
+	
+	for (int i = 1; i <= m; i++){
+		int u = d[i], v = c[i];
+		if (vs[u] == vs[v] && vs[u] != 0)
+			continue;
+		k++;
+		tree.pb(e[i]);
+		wt += e.ts;
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
